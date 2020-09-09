02ec71ad679bcc73376d1fd6e7b927c360098ce1
Freddie <freddie.williams@morehouse.edu>
2020-09-08 20:38:05 -0400

    WIP

---
+++ src/main/java/Controller/AmmoController.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AmmoController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
---
+++ src/test/java/com/example/RealAmmo/RealAmmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealAmmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealAmmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
